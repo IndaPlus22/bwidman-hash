4d8cf9f291ea8ebbeba8f3ca55ff9f40554be1e3
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-02-07 20:54:04 +0100

    Working hashing (collisions)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 use std::io;
 
-struct HashMap<T> {
-    keys: Vec<T>,
-    values: Vec<T>,
+#[derive(Clone)]
+struct Element<T>
+where T: Copy {
+    key: String,
+    value: T
 }
 
-impl<T> HashMap<T> {
+struct HashMap<T>
+where T: Copy {
+    elements: Vec<Option<Element<T>>>
+}
+
+impl<T> HashMap<T>
+where T: Copy {
     fn new() -> Self {
         const START_CAPACITY: usize = 50;
-        Self {
-            keys: Vec::with_capacity(START_CAPACITY),
-            values: Vec::with_capacity(START_CAPACITY),
+        
+        Self { elements: vec![None; START_CAPACITY] }
+    }
+
+    fn insert(&mut self, key: &str, value: T) {
+        const MAX_CAPACITY: f32 = 0.75;
+
+        let index = self.hash_code(key);
+        self.elements[index] = Some(Element{ key: key.to_string(), value });
+    }
+
+    fn delete(&mut self, key: &str) {
+        let index = self.hash_code(key);
+        self.elements[index] = None;
+    }
+
+    fn search(&mut self, key: &str) -> Option<T> {
+        let index = self.hash_code(key);
+
+        return match self.elements[index].clone() {
+            Some(e) => Some(e.value),
+            None => None
         }
     }
 
-    fn insert(&mut self, key: T, value: T) {
-        todo!()
-    }
-
-    fn delete(&mut self, key: T) {
-        todo!()
-    }
-
-    fn search(&mut self, key: T) -> T {
-        todo!()
+    fn hash_code(&self, key: &str) -> usize {
+        let key_value: usize = key.as_bytes()
+            .iter()
+            .map(|x| *x as usize)
+            .sum();
+        return key_value * 17 % self.elements.len();
     }
 }
 
@@ -31,21 +54,22 @@ fn main() {
     // Get stored database file
     let mut data: HashMap<u32> = HashMap::new();
 
-    println!("Welcome bwidman's secret database!");
+    println!("Welcome to bwidman's secret database!");
 
     loop {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-
-        if command == "quit" {
+        
+        if command.trim() == "quit" {
             break;
         }
 
-        let parameters: Vec<&str> = command.split(' ').collect();
+        let parameters: Vec<&str> = command.trim().split(' ').collect();
+        
         match parameters[0] {
-            "insert" => todo!(),
-            "delete" => todo!(),
-            "select" => todo!(),
+            "insert" => data.insert(parameters[1], parameters[2].parse::<u32>().unwrap()),
+            "delete" => data.delete(parameters[1]),
+            "select" => println!("Matched result: {}", data.search(parameters[1]).unwrap()),
             _ => (),
         }
     }
