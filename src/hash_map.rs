3f2638ba186f281129fc6ee447e7f739346af195
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-02-09 01:45:44 +0100

    Resizeable hash map, start implementation of Table

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -1,58 +1,84 @@
 #[derive(Clone)]
 pub struct Element<T>
-where T: Copy {
+where T: Clone {
     key: String,
-    value: T
+    value: T,
 }
 
 pub struct HashMap<T>
-where T: Copy {
-    slots: Vec<Vec<Element<T>>>
+where T: Clone {
+    slots: Vec<Vec<Element<T>>>,
+    fullness: f32,
 }
 
 impl<T> HashMap<T>
-where T: Copy {
+where T: Clone {
     pub fn new() -> Self {
         const START_CAPACITY: usize = 32;
         
-        Self { slots: vec![vec![]; START_CAPACITY] }
+        Self { slots: vec![vec![]; START_CAPACITY], fullness: 0.0 }
     }
 
     pub fn insert(&mut self, key: &str, value: T) {
         const MAX_CAPACITY: f32 = 0.75;
 
-        // First, if hash map if getting almost full (ratio of used slots > MAX_CAPACITY)
-        // resize vector
-        
+        // First, if hash map if getting almost full, resize vector
+        if self.fullness > MAX_CAPACITY {
+            let old_slots = self.slots.clone();
+
+            self.slots = vec![vec![]; self.slots.len() * 2]; // New vector with double the size
+            self.fullness = 0.0;
+
+            // Insert all old entries into new vector
+            for slot in old_slots {
+                for element in slot {
+                    self.insert(element.key.as_str(), element.value);
+                }
+            }
+        }
         
         let index = self.hash_code(key);
+        
+        // Increase fullness if new slot is used
+        if self.slots[index].len() == 0 {
+            self.fullness += 1.0 / self.slots.len() as f32;
+        }
+
         self.slots[index].push(Element{ key: key.to_string(), value });
     }
 
     pub fn delete(&mut self, key: &str) {
         let index = self.hash_code(key);
 
-        // Search for the correct element to remove among
-        // potential collisions in the same hash map slot
+        // Search for the correct element to remove
+        // among potential collisions in the same slot
         for (i, element) in self.slots[index].iter().enumerate() {
             if element.key == key {
                 self.slots[index].remove(i);
                 return;
             }
         }
+        println!("{key} does not exist");
     }
 
-    pub fn search(&mut self, key: &str) -> Option<T> {
+    pub fn search(&self, key: &str) -> Option<&T> {
         let index = self.hash_code(key);
 
-        // Search for the correct element to return among
-        // potential collisions in the same hash map slot
+        // Search for the correct element to return
+        // among potential collisions in the same slot
         for (i, element) in self.slots[index].iter().enumerate() {
             if element.key == key {
-                return Some(self.slots[index][i].value);
+                return Some(&self.slots[index][i].value);
             }
         }
         None // No element of the key was found
+    }
+
+    pub fn contains(&self, key: &str) -> bool {
+        match self.search(key) {
+            Some(x) => true,
+            None => false,
+        }
     }
 
     pub fn get_elements(&self) -> Vec<Element<T>> {
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,51 @@ mod hash_map;
 use std::io;
 use hash_map::HashMap;
 
+type Entry = Vec<String>;
+
+struct Table {
+    headers: Entry, // Titles for every column header
+    entry_indices: Vec<HashMap<Vec<usize>>>, // Hash maps for every column, containing indices for the respective entries having that value in that column
+    // Problem: during insertion, must use search() to get reference to Vec of indices then push(index), if HashMap::contains(col_value)
+    entries: Vec<Entry>, // Entries with values for every column
+}
+
+impl Table {
+    fn new(file_path: &str) -> Self {
+        // Open file
+
+
+        // Read data
+        // First line contains the headers
+
+        // Every row is an entry with comma separated column values
+
+
+        Self { headers, entry_indices: , entries:  }
+    }
+    
+    fn select(&self, parameters: Vec<&str>) {
+        // Only "select", just print all entries
+        if parameters.len() == 1 {
+            for entry in self.entries {
+                print_entry(entry);
+            }
+        }
+
+        println!("Matched result: {}", data.search(parameters[1]).unwrap());
+    }
+}
+
+fn print_entry(entry: Entry) {
+    for value in entry {
+        print!("{value}\t");
+    }
+    println!();
+}
+
 fn main() {
     // Get stored database file
-    let mut data: HashMap<u32> = HashMap::new();
+    let mut data = Table::new("data.csv");
 
     println!("Welcome to bwidman's secret database!");
 
@@ -22,7 +64,8 @@ fn main() {
         match parameters[0] {
             "insert" => data.insert(parameters[1], parameters[2].parse::<u32>().unwrap()),
             "delete" => data.delete(parameters[1]),
-            "select" => println!("Matched result: {}", data.search(parameters[1]).unwrap()),
+            "select" => data.select(parameters),
+            "create" => todo!(),
             _ => (),
         }
     }
